2bcfa3bfce8218c790898648a9931fb0c91d35cf
Jose Luis Feiteirinha <feiteira@gmail.com>
2017-08-31 13:06:42 +0200

    Arduino code now reading from Serial

--- Arduino/test_motor1/test_motor1.ino
+++ Arduino/test_motor1/test_motor1.ino
@@ -21,40 +21,62 @@ int incomingByte = 0;   // for incoming serial data
 
 
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        Serial.begin(115200);     // opens serial port, sets data rate to 9600 bps
 
   
   stepper1.setMaxSpeed(1200.0);
-  stepper1.setAcceleration(100.0);
-  stepper1.setSpeed(200);
+  stepper1.setAcceleration(200.0);
+  stepper1.setSpeed(400);
   //4076 
-  stepper1.moveTo(4076);
+ // stepper1.moveTo(4076);
 
   stepper2.setMaxSpeed(1200.0);
-  stepper2.setAcceleration(100.0);
-  stepper2.setSpeed(200);
+  stepper2.setAcceleration(200.0);
+  stepper2.setSpeed(400);
   //4076 
-  stepper2.moveTo(-4076);
+ // stepper2.moveTo(-4076);
 
 
 
 }//--(end setup )---
 
+void processSerial(){
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+     // read the incoming byte:
+     incomingByte = Serial.read();
+     // say what you got:
+     Serial.print("I received: ");
+     Serial.println(incomingByte, DEC);
+    stepper1.moveTo(incomingByte);
+    stepper2.moveTo(-incomingByte);
+
+ 
+   }
+}
+
 void loop() {
 
   //Change direction when the stepper reaches the target position
-//  if (stepper1.distanceToGo() == 0) {
-//    stepper1.moveTo(-stepper1.currentPosition());
-//  }
+  if (stepper1.distanceToGo() != 0) {
+    //  Serial.print("Stepper 1: ");
+    Serial.print(stepper1.distanceToGo(), DEC);
+    //  Serial.print("\tStepper 2: ");
+    Serial.println(stepper2.distanceToGo(), DEC);
+    //    stepper1.moveTo(-stepper1.currentPosition());
+  }else{
+    String str = Serial.readStringUntil("\n");
+    if(str.length() != 0){
+      int m1, m2;
+      sscanf (str.c_str(), "%d %d", &m1, &m2);
+      Serial.print(m1, DEC);
+       Serial.print(m2, DEC);
+      stepper1.moveTo(m1);
+      stepper2.moveTo(m2);
+    }
+  }
   stepper1.run();
   stepper2.run();
- // send data only when you receive data:
-        if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.read();
 
-                // say what you got:
-                Serial.print("I received: ");
-                Serial.println(incomingByte, DEC);
-        }
+ //processSerial();
 }
